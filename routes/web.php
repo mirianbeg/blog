a22d3dc0f0cb0ce1d80b8de9c215c4cee5bcf5bd
mirianbeg <meraburushadze@gmail.com>
2017-07-24 00:30:30 +0400

    finished 8th lesson

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,16 @@
 
 use App\Task;
 
-Route::get('/tasks', function () {
-	//$tasks = DB::table('tasks')->get();
-	$tasks = Task::all();
-	//return $tasks;
-   return view('tasks.index', compact('tasks'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('//tasks/{task}', 'TasksController@show');
 
-Route::get('/tasks/{task}', function ($id) {
-	//dd($id);
-	//$task = DB::table('tasks')->find($id);
-	$task = Task::find($id);
-   return view('tasks.show', compact('task'));
-});
+
+// Route::get('/tasks/{task}', function ($id) {
+// 	//dd($id);
+// 	//$task = DB::table('tasks')->find($id);
+// 	$task = Task::find($id);
+//    return view('tasks.show', compact('task'));
+// });
 
 
 
