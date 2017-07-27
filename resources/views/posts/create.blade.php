c9afbf015419fec8cf10c27336d119de6ac5547f
mirianbeg <meraburushadze@gmail.com>
2017-07-27 21:05:57 +0400

    finished 12th lesson;

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,17 +8,31 @@
 {{csrf_field()}}
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="title">
+    <input type="text" class="form-control" id="title" name="title" >
   </div>
 
   <div class="form-group">
     <label for="body">Body</label>
-    <input type="textarea" class="form-control" id="body" name="body">
+    <input type="textarea" class="form-control" id="body" name="body" >
   </div>
 
-  
- <button type="submit" class="btn btn-primary">Submit</button>
+  <div class="form-group">
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
  </form>
+
+@if(count($errors))
+ <div class="form-group">
+  <div class="alert alert-danger">
+   <ul>
+     @foreach ($errors->all() as $error)
+      <li> {{$error}} </li>
+     @endforeach
+   </ul>
+ </div>
+</div>
+@endif
+
 </div>
 
        @include('layouts.sidebar')
